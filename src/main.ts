b1d6316dc3aec35aaf6709e5f1182cde3a90ac7f
Tesao <shimeji9@gmail.com>
2025-11-30 22:18:45 +0900

    add help link

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,9 @@ import './style.css';
 import './components/board-grid.ts'; // Import the Web Component
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
+  <div style="margin-bottom: 1.5rem;">
     <h1>Keishi Board Game</h1>
-    <board-grid></board-grid>
+    <a class="help-link" href="https://raw.githubusercontent.com/tessai9/keishi/refs/heads/main/docs/official_rule_book.md" target="_blank">Help (Official Rule Book)</a>
   </div>
+  <board-grid></board-grid>
 `;
